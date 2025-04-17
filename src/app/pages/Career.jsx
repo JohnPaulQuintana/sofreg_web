f5f3f209d0b60785cf710c427e431b58e322f796
John Paul Quintana <jpquintana2024@gmail.com>
2025-04-17 19:08:52 +0800

    Update Career.jsx

--- src/app/pages/Career.jsx
+++ src/app/pages/Career.jsx
@@ -7,8 +7,9 @@ import CareerHeader from "../components/Career/CareerHeader";
 import SectionHeader from "../components/Career/SectionHeader";
 import CareerMarqueeSection from "../components/Career/CareerMarquee";
 import SectionContent from "../components/Career/SectionContent";
-import jobOverlay from "../components/Popup/JobOverlay.jsx";
+// import jobOverlay from "../components/Popup/JobOverlay.jsx";
 import FooterDetailed from "../components/FooterDetailed";
+import JobOverlay from "../components/Popup/JobOverlay.jsx";
 const Career = () => {
 
     const [isModalVisible, setIsModalVisible] = useState(false);
@@ -50,7 +51,7 @@ const Career = () => {
 
             <FooterDetailed />
             </div>
-            <jobOverlay isVisible={isModalVisible} details={selectedJob} onClose={closeModal}/>
+            <JobOverlay isVisible={isModalVisible} details={selectedJob} onClose={closeModal}/>
         </div>
     )
 }
